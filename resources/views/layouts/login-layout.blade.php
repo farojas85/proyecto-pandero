a4ee6f29f5ec02ab5f85a17447e3783e94fcb913
Fredy Rojas <farojas851@gmail.com>
2022-02-24 23:01:58 -0500

    Modificando Assets en Login

--- resources/views/layouts/login-layout.blade.php
+++ resources/views/layouts/login-layout.blade.php
@@ -12,21 +12,21 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="author" content="FarbeSofts">
     <title>@yield('title') | Pandero</title>
-    <link rel="apple-touch-icon" href="{{ asset('app-assets/images/favicon/apple-touch-icon-152x152.png') }}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('app-assets/images/favicon/favicon-32x32.png') }}">
+    <link rel="apple-touch-icon" href="app-assets/images/favicon/apple-touch-icon-152x152.png">
+    <link rel="shortcut icon" type="image/x-icon" href="app-assets/images/favicon/favicon-32x32.png">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- BEGIN: VENDOR CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/vendors.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="app-assets/vendors/vendors.min.css">
     <!-- END: VENDOR CSS-->
     <!-- BEGIN: Page Level CSS-->
     <link rel="stylesheet" type="text/css"
-        href="{{ asset('app-assets/css/themes/vertical-modern-menu-template/materialize.css') }}">
+        href="app-assets/css/themes/vertical-modern-menu-template/materialize.css">
     <link rel="stylesheet" type="text/css"
-        href="{{ asset('app-assets/css/themes/vertical-modern-menu-template/style.css') }}">
+        href="app-assets/css/themes/vertical-modern-menu-template/style.css">
     <link rel="stylesheet" type="text/css" href="app-assets/css/pages/login.css">
     <!-- END: Page Level CSS-->
     <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/custom/custom.css') }}">
+    <link rel="stylesheet" type="text/css" href="app-assets/css/custom/custom.css">
     <!-- END: Custom CSS-->
 </head>
 <!-- END: Head-->
@@ -44,14 +44,14 @@
     </div>
 
     <!-- BEGIN VENDOR JS-->
-    <script src="{{ asset('app-assets/js/vendors.min.js') }}"></script>
+    <script src="app-assets/js/vendors.min.js"></script>
     <!-- BEGIN VENDOR JS-->
     <!-- BEGIN PAGE VENDOR JS-->
     <!-- END PAGE VENDOR JS-->
     <!-- BEGIN THEME  JS-->
-    <script src="{{ asset('app-assets/js/plugins.js') }}"></script>
-    <script src="{{ asset('app-assets/js/search.js') }}"></script>
-    <script src="{{ asset('app-assets/js/custom/custom-script.js') }}"></script>
+    <script src="app-assets/js/plugins.js"></script>
+    <script src="app-assets/js/search.js"></script>
+    <script src="app-assets/js/custom/custom-script.js"></script>
     <!-- END THEME  JS-->
     <!-- BEGIN PAGE LEVEL JS-->
     <!-- END PAGE LEVEL JS-->
